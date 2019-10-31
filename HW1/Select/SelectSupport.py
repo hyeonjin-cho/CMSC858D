62adb488cab1e3121b4a97710901c77acec6d228
hyeonjin-cho <57103534+hyeonjin-cho@users.noreply.github.com>
2019-10-31 09:00:58 -0400

    Update SelectSupport.py

--- HW1/Select/SelectSupport.py
+++ HW1/Select/SelectSupport.py
@@ -1,7 +1,7 @@
 ########################################################################
 # Author: Hyeon Jin Cho
 # CMSC858D_HW1: Implementing bit vector select
-# Last updated: 10/26/2019
+# Last updated: 10/30/2019
 ########################################################################
 
 # Packages installed
@@ -10,11 +10,13 @@ from RankSupport import RankSupport
 from itertools import product
 import math
 
-
 class SelectSupport():
 	def __init__(self, bv, size):
+		self.bv = bv
+
 		# size of bit-vector
 		self.SIZE = size
+
 		# sizes of superblock and block
 		self.SIZE_S = int(math.ceil((math.log2(self.SIZE)**2)/2))
 		self.SIZE_B = int(math.ceil(math.log2(self.SIZE)/2))
@@ -27,57 +29,38 @@ class SelectSupport():
 		self.NUM_S = int(math.ceil(self.SIZE/self.SIZE_S))
 		self.NUM_B = int(math.ceil(self.SIZE/self.SIZE_B))
 
-		self.rank = self.callRankSupport(bv, size)
-		#return rank
-
-	def callRankSupport(self, bv, size):
-		self.R_s = RankSupport.rank_s(self, bv)
-		self.R_b = RankSupport.rank_b(self, bv)
-		self.R_p = RankSupport.rank_p(self, bv)
-
-		return self.R_s, self.R_b, self.R_p
+		self.rank = RankSupport(bv, size)
 
 	def countBits(self, bv, start, end):
 		bv = bv[start:end]
 		count = bv.count_bits()
 		return count
 
-	def getRank(self, bv, rank, size, index_count):
-		n = bv[0:size]
-		
+	def getRank(self, rank, size, index_count):
 		if size == 2:
-			if RankSupport.rank_indexAt(self, bv, int(math.ceil(size/2)+index_count)) == rank:
+			if RankSupport.rank_indexAt(self.rank, self.bv, int(math.ceil(size/2)+index_count)) == rank:
 				return int(math.ceil(size/2)+index_count)
 			else:
 				return index_count
-		elif RankSupport.rank_indexAtforSelect(self, bv, int(math.ceil(size/2)+index_count)) > rank:
-			n = n[0:int(math.ceil(size/2))]
+		elif RankSupport.rank_indexAtforSelect(self.rank, self.bv, int(math.ceil(size/2)+index_count)) > rank:
 			index_count = index_count
 			size = int(math.ceil(size/2))
-			return self.getRank(bv, rank, size, index_count)
-		elif RankSupport.rank_indexAtforSelect(self, bv, int(math.ceil(size/2)+index_count)) < rank:
-			n = n[int(math.ceil(size/2)):size]
+			return self.getRank(rank, size, index_count)
+		elif RankSupport.rank_indexAtforSelect(self.rank, self.bv, int(math.ceil(size/2)+index_count)) < rank:
 			index_count = int(math.ceil(size/2)+index_count)
 			size = int(math.ceil(size/2))
-			return self.getRank(bv, rank, size, index_count)
-		elif RankSupport.rank_indexAtforSelect(self, bv, int(math.ceil(size/2)+index_count)) == rank:
-			if RankSupport.rank_indexAt(self, bv, int(math.ceil(size/2)+index_count)) == rank:
+			return self.getRank(rank, size, index_count)
+		elif RankSupport.rank_indexAtforSelect(self.rank, self.bv, int(math.ceil(size/2)+index_count)) == rank:
+			if RankSupport.rank_indexAt(self.rank, self.bv, int(math.ceil(size/2)+index_count)) == rank:
 				return int(math.ceil(size/2)+index_count)
 			else:
-				n = n[0:int(math.ceil(size/2))]
 				index_count = index_count
 				size = int(math.ceil(size/2))
-				return self.getRank(bv, rank, size, index_count)
+				return self.getRank(rank, size, index_count)
 
 	def select_rankOf(self, bv, rank):
-		s = self.R_s
-		b = self.R_b
-		p = self.R_p
-
-		a = self.rank
-
 		if rank > bv.count_bits():
 			return 'NA'
 		else:
-			return self.getRank(bv, rank, self.SIZE, 0)
+			return self.getRank(rank, self.SIZE, 0)
 
