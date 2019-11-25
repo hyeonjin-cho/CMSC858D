28d9b20304204c4ae6affbd6d5ad58fc45622e6b
Hyeon Jin Cho <57103534+hyeonjin-cho@users.noreply.github.com>
2019-11-25 15:30:20 -0500

    Add files via upload

---
+++ HW2/BloomFilter/BFbuild.py
@@ -0,0 +1,32 @@
+########################################################################
+# Author: Hyeon Jin Cho
+# CMSC858D_HW2: Bloom filter build class
+# Last updated: 11/22/2019
+########################################################################
+
+# Packages installed
+import math
+import bitarray
+import mmh3
+
+class BFbuild():
+	def __init__(self, fpr, numKeys):
+		self.p = fpr
+		self.n = numKeys
+		self.m = math.ceil((-self.n*math.log(self.p))/(math.log(2)**2))
+		self.k = math.ceil((self.m/self.n)*math.log(2))
+
+		#print("fpr: ", self.p)
+		#print("num of keys: ", self.n)
+		#print("size of bitarray: ", self.m)
+		#print("num of hash functions: ", self.k)
+
+		self.bitarray = bitarray.bitarray(self.m)
+		self.bitarray.setall(0)
+
+	def insert(self, item):
+		for i in range(self.k):
+			index = mmh3.hash(item, i) % (self.m)
+			self.bitarray[index] = '1'
+		return self.bitarray
+
