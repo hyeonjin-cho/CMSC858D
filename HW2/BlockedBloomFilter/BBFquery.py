0f33599b7b3944f85028025f353ca6382b3d818e
Hyeon Jin Cho <57103534+hyeonjin-cho@users.noreply.github.com>
2019-11-26 22:58:16 -0500

    Add files via upload

---
+++ HW2/BlockedBloomFilter/BBFquery.py
@@ -0,0 +1,31 @@
+########################################################################
+# Author: Hyeon Jin Cho
+# CMSC858D_HW2: Blocked Bloom filter query class
+# Last updated: 11/25/2019
+########################################################################
+
+# Packages installed
+import math
+import bitarray
+import mmh3
+
+class BBFquery():
+	def __init__(self, m, k, b, queryData, bitList):
+		self.m = m
+		self.k = k
+		self.b = b
+		self.queryData = queryData
+		self.bitList = bitList
+
+		self.queryOut = self.query(self.queryData, self.bitList)
+
+	def query(self, item, bitList):
+		setBool = True # initially set to True because false positives are allowed
+		block = mmh3.hash(item, 0) % (self.b)
+		for i in range(1, self.k):
+			index = mmh3.hash(item, i) % 512
+			if bitList[block][index] == '0':
+				setBool = False
+				return setBool
+		return setBool
+
